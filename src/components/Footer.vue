dff358b094b482f2ae908050fcef441902737133
Tiiduke <tiithendrik.piibeleht@gmail.com>
2019-11-10 19:50:31 +0200

    Added Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <ul class="links footer">
+            <li>
+                <a href="https://ois2.ut.ee/" target="_blank">OIS</a>
+            </li>
+            <li>
+                <a href="https://courses.cs.ut.ee/" target="_blank">Courses</a>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
